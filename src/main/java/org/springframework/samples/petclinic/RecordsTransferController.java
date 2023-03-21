1dfcb93369ada04ad30cad1e6960017ff7bd1ece
Johnathan Gilday <me@johnathangilday.com>
2023-03-20 21:46:05 -0400

    ⚗️ add zip overflow example

---
+++ src/main/java/org/springframework/samples/petclinic/RecordsTransferController.java
@@ -0,0 +1,29 @@
+package org.springframework.samples.petclinic;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.zip.ZipInputStream;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+class RecordsTransferController {
+
+	@PostMapping("/records-transfers")
+	public void newRecordsTransfer(@RequestParam("zipped") boolean zipped, @RequestBody InputStream body)
+			throws IOException {
+		final InputStream is = zipped ? new ZipInputStream(body) : body;
+		try (is) {
+			saveToRecordsSystem(is);
+		}
+	}
+
+	private void saveToRecordsSystem(final InputStream is) throws IOException {
+		// TODO
+		is.transferTo(OutputStream.nullOutputStream());
+	}
+
+}
