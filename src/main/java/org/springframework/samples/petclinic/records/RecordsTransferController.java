f3fb655cc7aaf73a0cfa8096ecf01a2be167c351
Johnathan Gilday <me@johnathangilday.com>
2023-03-20 22:33:36 -0400

    ✨ Save New Records to File System

--- src/main/java/org/springframework/samples/petclinic/records/RecordsTransferController.java
+++ src/main/java/org/springframework/samples/petclinic/records/RecordsTransferController.java
@@ -3,7 +3,13 @@ package org.springframework.samples.petclinic.records;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.zip.ZipInputStream;
+import javax.xml.stream.XMLEventReader;
+import javax.xml.stream.XMLInputFactory;
+import javax.xml.stream.XMLStreamException;
+import javax.xml.stream.events.XMLEvent;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -11,6 +17,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 class RecordsTransferController {
+
+	private final Path recordsDir = Path.of("/tmp/records");
 
 	@PostMapping("/records-transfers")
 	public void newRecordsTransfer(@RequestParam("zipped") boolean zipped, @RequestBody InputStream body)
@@ -22,8 +30,9 @@ class RecordsTransferController {
 	}
 
 	private void saveToRecordsSystem(final InputStream is) throws IOException {
-		// TODO
-		is.transferTo(OutputStream.nullOutputStream());
+		final var path = recordsDir.resolve("new-record.xml");
+		final var os = Files.newOutputStream(path);
+		is.transferTo(os);
 	}
 
 }
