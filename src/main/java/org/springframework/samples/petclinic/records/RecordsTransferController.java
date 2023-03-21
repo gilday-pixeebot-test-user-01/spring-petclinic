4f490242dbbe81c7eca67527828643c601f263fa
Johnathan Gilday <me@johnathangilday.com>
2023-03-20 22:33:36 -0400

    ⚗️ introduce unsafe XML parsing

--- src/main/java/org/springframework/samples/petclinic/records/RecordsTransferController.java
+++ src/main/java/org/springframework/samples/petclinic/records/RecordsTransferController.java
@@ -1,9 +1,13 @@
-package org.springframework.samples.petclinic;
+package org.springframework.samples.petclinic.records;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.util.zip.ZipInputStream;
+import javax.xml.stream.XMLEventReader;
+import javax.xml.stream.XMLInputFactory;
+import javax.xml.stream.XMLStreamException;
+import javax.xml.stream.events.XMLEvent;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -22,7 +26,29 @@ class RecordsTransferController {
 	}
 
 	private void saveToRecordsSystem(final InputStream is) throws IOException {
-		// TODO
+		final var factory = XMLInputFactory.newFactory();
+		final XMLEventReader reader;
+		try {
+			reader = factory.createXMLEventReader(is);
+		}
+		catch (XMLStreamException e) {
+			throw new IOException("Failed to read XML", e);
+		}
+		while (reader.hasNext()) {
+			final XMLEvent event;
+			try {
+				event = reader.nextEvent();
+			}
+			catch (XMLStreamException e) {
+				throw new IOException("Failed to read XML", e);
+			}
+			if (event.getEventType() == XMLEvent.START_ELEMENT) {
+				final var startElement = event.asStartElement();
+				if (!startElement.getName().getLocalPart().equals("ExternalVetRecord")) {
+					throw new IOException("XML does not match expected schema");
+				}
+			}
+		}
 		is.transferTo(OutputStream.nullOutputStream());
 	}
 
