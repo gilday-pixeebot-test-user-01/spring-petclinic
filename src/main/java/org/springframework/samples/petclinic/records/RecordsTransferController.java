bbb0e8189512beaf9bae86d1188bd1b8f88c702a
pixee <help@pixee.ai>
2023-03-22 20:23:44 +0000

    Updated RecordsTransferController.java:37 - Created a hardened `java.io.ZipInputStream` wrapper type that prevents files from being written that escape the target directory
    [Rules]
     - pixee:java/harden-zip-entry-paths

--- src/main/java/org/springframework/samples/petclinic/records/RecordsTransferController.java
+++ src/main/java/org/springframework/samples/petclinic/records/RecordsTransferController.java
@@ -1,5 +1,6 @@
 package org.springframework.samples.petclinic.records;
 
+import io.openpixee.security.ZipSecurity;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.file.Files;
@@ -34,7 +35,7 @@ class RecordsTransferController {
 	@PostMapping("/records-transfers")
 	public NewRecordModel newRecordsTransfer(@RequestParam("zipped") boolean zipped, @RequestBody InputStream body)
 			throws IOException {
-		final InputStream is = zipped ? new ZipInputStream(body) : body;
+		final InputStream is = zipped ? ZipSecurity.createHardenedInputStream(body) : body;
 		final String id;
 		try (is) {
 			id = saveToRecordsSystem(is);
