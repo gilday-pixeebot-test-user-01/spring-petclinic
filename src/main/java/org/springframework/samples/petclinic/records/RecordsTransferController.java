40351ceab9eba868e56c35f608298d206024b2ac
Johnathan Gilday <me@johnathangilday.com>
2023-03-22 11:18:23 -0400

    ✨ save record in database

--- src/main/java/org/springframework/samples/petclinic/records/RecordsTransferController.java
+++ src/main/java/org/springframework/samples/petclinic/records/RecordsTransferController.java
@@ -2,14 +2,13 @@ package org.springframework.samples.petclinic.records;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.OutputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.zip.ZipInputStream;
-import javax.xml.stream.XMLEventReader;
-import javax.xml.stream.XMLInputFactory;
-import javax.xml.stream.XMLStreamException;
-import javax.xml.stream.events.XMLEvent;
+import javax.sql.DataSource;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -18,21 +17,32 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 class RecordsTransferController {
 
-	private final Path recordsDir = Path.of("/tmp/records");
+	private final Path recordsDir;
+
+	private final DataSource dataSource;
+
+	@Autowired
+	RecordsTransferController(final DataSource dataSource) {
+		recordsDir = Path.of("records");
+		this.dataSource = dataSource;
+	}
 
 	@PostMapping("/records-transfers")
 	public void newRecordsTransfer(@RequestParam("zipped") boolean zipped, @RequestBody InputStream body)
-			throws IOException {
+			throws IOException, SQLException {
 		final InputStream is = zipped ? new ZipInputStream(body) : body;
 		try (is) {
 			saveToRecordsSystem(is);
 		}
 	}
 
-	private void saveToRecordsSystem(final InputStream is) throws IOException {
-		final var path = recordsDir.resolve("new-record.xml");
-		final var os = Files.newOutputStream(path);
-		is.transferTo(os);
+	private void saveToRecordsSystem(final InputStream is) throws IOException, SQLException {
+		final var path = recordsDir.resolve("records.json");
+		Files.copy(is, path);
+		final var connection = dataSource.getConnection();
+		final PreparedStatement statement = connection.prepareStatement("INSERT INTO records (path) VALUES ?");
+		statement.setString(1, path.toString());
+		statement.execute();
 	}
 
 }
