2051faed867637b388a6ed80dc9b34513e3d4151
gilday-pixeebot-test-user-01 <123498103+gilday-pixeebot-test-user-01@users.noreply.github.com>
2023-11-13 12:56:58 -0500

    Update 1 to BaseEntity.java

--- src/main/java/org/springframework/samples/petclinic/model/BaseEntity.java
+++ src/main/java/org/springframework/samples/petclinic/model/BaseEntity.java
@@ -36,6 +36,7 @@ public class BaseEntity implements Serializable {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 
+	/** @return the ID */
 	public Integer getId() {
 		return id;
 	}
